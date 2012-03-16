c3acc372ce3fd28c3f5606f121f0cd17ca5f48db
unknown <dmitriy@volganet.org>
2012-03-16 22:58:49 +0400

    Issue #0 by DmitriyMakeev: Initial release.

---
+++ russian-beauty-calendar-day.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Theme implementation to display day cell.
+ *
+ * Variables:
+ * - $number: Day number from beginning of month
+ * - $date: Day number from beginning of month
+ * - $delta: Day number from beginning of week
+ * - $class: Default cell class
+ * - $count: Node counter
+ * - $using_tooltip: Using tooltips (boolean)
+ * - $is_empty: Blank cell (boolean)
+ */
+?>
+
+<?php if($is_empty) $class .= ' blank'; ?>
+
+<?php if ($count > 0) print '<a class="tooltip" title="'.russian_beauty_calendar_plural($count). '"' . ($using_tooltip ? ' rel="'.$date.'"' : '' ) . ' href="/calendar/'.$date.'">'; ?>
+  <div class="<?php print $class; ?>"><div class="rbc_value"><?php print $number; ?></div></div>
+<?php if($count > 0) print '</a>'; ?>
