5d6cc30751fb4b111ab50ef99572297f7b36537a
laryn <laryn@cedc.org>
2021-03-31 16:32:24 -0500

    Initial version of new Basis theme

--- js/pretty_calendar.js
+++ js/pretty_calendar.js
@@ -4,7 +4,6 @@ function calendar_go(dir) {
     $.get(Backdrop.settings.basePath + "calendar_get/" + $(".calendar-" + dir).parent().attr("rel"), function(data){
       $("#calendar-body").html(" ");
       $("#calendar-body").append(data);
-      $(".tooltip").tipTip({defaultPosition: "right", maxWidth: "400px", keepAlive:true});
       $("#calendar-body .calendar-daynames .calendar-value").each(function(){
         $(this).html($(this).html().substr(0,3));
       });
